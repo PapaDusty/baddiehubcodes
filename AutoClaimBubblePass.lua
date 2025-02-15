41e27abe8d8fe545cd0e4885ff92993eaa4e531b
PapaDusty <cyknseppuku@gmail.com>
2025-02-15 16:13:11 -0700

    Create AutoClaimBubblePass.lua

---
+++ AutoClaimBubblePass.lua
@@ -0,0 +1,143 @@
+      local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 1,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 2,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 3,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 4,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 5,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 6,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 7,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 8,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 9,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 10,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 11,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 12,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 13,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 14,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 15,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 16,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 17,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
+      wait(0.2)
+	  local remote = game:GetService("ReplicatedStorage").NetworkRemoteEvent
+      local arguments = {
+        [1] = "ClaimChallengeReward",
+        [2] = 18,
+        [3] = false
+      }
+      remote:FireServer(unpack(arguments))
